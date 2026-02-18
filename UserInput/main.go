365e214c5905af80fa0cdf3ac4c498077a24b2e1
Sagar <sagarwaghmare1384@gmail.com>
2026-02-18 23:15:16 +0530

    Time,memory allocation and pointers

--- UserInput/main.go
+++ UserInput/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
@@ -18,4 +19,29 @@ func main() {
 	input,_:=reader.ReadString('\n')
 	fmt.Println("Thanks for rating,",input)
 	fmt.Printf("Type of this rating is %T",input)
+
+	// Time how to use it
+	fmt.Println("Here we see how to use time")
+	presentTime:=time.Now()
+	fmt.Println(presentTime)
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+    createdDate:=time.Date(2020,time.August,19,3,4,2,5,time.UTC)
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday")) 
+	//for building we directly write go build
+	// There is like different go os
+	//GOOS='darwin' =mac and then go build 
+	//GOOS='windows' go build
+	//GOOS='linux' go build
+	// in this way we get the .exe file to execute
+	
+	//memory mangement
+	//new() and make()
+	// new() allocate you will get a memory address and zero storage
+	// make() you will get a memory address and non-zero storage
+	// Garbage collections happens automatically
+
+
+
+
 }
